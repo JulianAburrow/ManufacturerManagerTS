8310cf7845f47ea51f51e6950d7734719b4474b9
Julian Aburrow <Julian.Aburrow@powwr.co.uk>
2022-06-22 15:42:04 +0100

    Corrected WidgetConfiguration class

--- DataAccess/ConfigurationClasses/WidgetConfiguration.cs
+++ DataAccess/ConfigurationClasses/WidgetConfiguration.cs
@@ -14,11 +14,21 @@ namespace ManufacturerManagerTS.DataAccess.ConfigurationClasses
                 .WithMany(e => e.Widgets)
                 .HasForeignKey(e => e.ManufacturerId)
                 .IsRequired(true)
-                .OnDelete(DeleteBehavior.NoAction);
+                .OnDelete(DeleteBehavior.Cascade);
             builder.HasOne(e => e.Colour)
                 .WithMany(e => e.Widgets)
                 .HasForeignKey(e => e.ColourId)
                 .IsRequired(false)
+                .OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(e => e.StaffMemberCreated)
+                .WithMany(e => e.WidgetCreated)
+                .HasForeignKey(e => e.CreatedById)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(e => e.StaffMemberUpdated)
+                .WithMany(e => e.WidgetUpdated)
+                .HasForeignKey(e => e.LastUpdatedById)
+                .IsRequired()
                 .OnDelete(DeleteBehavior.NoAction);
         }
     }
